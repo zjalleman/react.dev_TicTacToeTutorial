e253f0bfbf8243d91b45613e620ffc0aaa818960
zjalleman <zjalleman@gmail.com>
2025-02-17 19:37:59 +0000

    Added X state for the tiles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 function Square() {
-  return <button className="square">1</button>;
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    //console.log("clicked!");
+    setValue("X");
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
