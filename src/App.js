e299e0d900233995f25a3cb86f1e75177d4b4153
zjalleman <zjalleman@gmail.com>
2025-02-17 20:20:39 +0000

    Moved state control to Board

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
+function Square({ value, onSquareClick }) {
+  //const [value, setValue] = useState(null);
 
   function handleClick() {
-    //console.log("clicked!");
-    setValue("X");
+    console.log("clicked!");
+    //setValue("X");
   }
 
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
